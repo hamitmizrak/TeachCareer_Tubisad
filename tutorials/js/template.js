eae8c99f442e78d106b27eea003f0571437972ae
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-01 19:19:32 +0300

    cast

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -10,40 +10,40 @@ multiLine Comment
 // alert("pop-up");
 // window.alert("window alert");
 
-console.log("console.log")
-document.write("html yazdım"+"<br>")
+// console.log("console.log")
+// document.write("html yazdım"+"<br>")
 
-var obj={adi:"deneme"};
-console.log(obj)
-document.write(obj)
+// var obj={adi:"deneme"};
+// console.log(obj)
+// document.write(obj)
 
 // ""   ==>   ' '  ==>   ` `
 ///////////////////////////////////////////////////////////////
 //Variable
-var variable=44;
-document.write("<br>"+typeof variable);
-document.write("==>"+variable);
+// var variable=44;
+// document.write("<br>"+typeof variable);
+// document.write("==>"+variable);
 
-variable=55.63;
-document.write("<br>"+typeof variable);
-document.write("==>"+variable);
+// variable=55.63;
+// document.write("<br>"+typeof variable);
+// document.write("==>"+variable);
 
-variable=false;
-document.write("<br>"+typeof variable);
-document.write("==>"+variable);
+// variable=false;
+// document.write("<br>"+typeof variable);
+// document.write("==>"+variable);
 
 
-variable="Merhabalar";
-document.write("<br>"+typeof variable);
-document.write("==>"+variable);
+// variable="Merhabalar";
+// document.write("<br>"+typeof variable);
+// document.write("==>"+variable);
 
-document.writeln("<br>++++++++++++++++++++++++")
-//variable rules English variable use
-var rules=55;
-var rulesProperty=55; //camel Case
-var rulesProperty9632=55; 
-var $rulesProperty9632=55; 
-var _rulesProperty9632=55; 
+document.writeln("++++++++++++++++++++++++<br>")
+// //variable rules English variable use
+// var rules=55;
+// var rulesProperty=55; //camel Case
+// var rulesProperty9632=55; 
+// var $rulesProperty9632=55; 
+// var _rulesProperty9632=55; 
 
 //yazamazsın
 //var 66rules=55;
@@ -57,15 +57,24 @@ var _rulesProperty9632=55;
 
 //Operators
 // 1-) Logic Operators        ==> &&=VE  ||=VEYA
-// 2-) Mathematics Operators  ==> + - * / % 
+// 2-) Mathematics Operators  ==> + - * ** / % 
 // 3-) Comparations Operators ==> > >= < <=  !  == === 
 // 4-) increment deincrement  ==> x++ , ++x , y-- , --y
 
 ///////////////////////////////////////////////////////////////
 //kullanıcıdan veri almak
 //prompt ==> string olarak geliyor.
-var userData=prompt("lütfen bir kelime giriniz")
-document.write(userData);
+// var userData=prompt("lütfen bir kelime giriniz")
+// document.write(userData);
+
+///////////////////////////////////////////////////////////////
+//CAST: dönüştür
+//Number() ==> SAYI
+//String() ==> KELIME
+var resutl="14",result2="26";
+document.write(resutl+result2+"<br>");
+document.write( Number(resutl)  + Number(result2) );
+
 
 
 
