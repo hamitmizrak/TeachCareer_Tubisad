69040c0d498b53f1a80c35df1c913fea133c7a66
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-01 18:47:28 +0300

    document.write() ,console.log()

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -6,3 +6,15 @@ multiLine Comment
 
 //document: çalıştığımız sayfanın html-body taglerine erişmek
 //window: her şeyin atası en üstteki önemli bir yapı
+
+// alert("pop-up");
+// window.alert("window alert");
+
+console.log("console.log")
+document.write("html yazdım"+"<br>")
+
+var obj={adi:"deneme"};
+console.log(obj)
+document.write(obj)
+
+// ""   ==>   ' '  ==>   ` `
