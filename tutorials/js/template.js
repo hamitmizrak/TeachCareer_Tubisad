dc500ff5f0574372770be2f5dfee65494906c152
hamitmizrak@gmail.com <hamitmizrak@gmail.com>
2022-06-01 19:12:54 +0300

    prompt

--- tutorials/js/template.js
+++ tutorials/js/template.js
@@ -21,20 +21,52 @@ document.write(obj)
 ///////////////////////////////////////////////////////////////
 //Variable
 var variable=44;
-document.write("<br>"+typeof variable)
+document.write("<br>"+typeof variable);
 document.write("==>"+variable);
 
 variable=55.63;
-document.write("<br>"+typeof variable)
+document.write("<br>"+typeof variable);
 document.write("==>"+variable);
 
 variable=false;
-document.write("<br>"+typeof variable)
+document.write("<br>"+typeof variable);
 document.write("==>"+variable);
 
 
 variable="Merhabalar";
-document.write("<br>"+typeof variable)
+document.write("<br>"+typeof variable);
 document.write("==>"+variable);
+
+document.writeln("<br>++++++++++++++++++++++++")
+//variable rules English variable use
+var rules=55;
+var rulesProperty=55; //camel Case
+var rulesProperty9632=55; 
+var $rulesProperty9632=55; 
+var _rulesProperty9632=55; 
+
+//yazamazsın
+//var 66rules=55;
+//var &rules=55;
+//var üğşçöĞŞÇÖ=55; //
+
+///////////////////////////////////////////////////////////////
+// =   ATAMA
+// ==  EŞİT (Türe bakmıyor)  Examples => 4 == "4" (TRUE)
+// === EŞİT (Tür bakıyor)    Examples => 4 ==="4" (FALSE)
+
+//Operators
+// 1-) Logic Operators        ==> &&=VE  ||=VEYA
+// 2-) Mathematics Operators  ==> + - * / % 
+// 3-) Comparations Operators ==> > >= < <=  !  == === 
+// 4-) increment deincrement  ==> x++ , ++x , y-- , --y
+
+///////////////////////////////////////////////////////////////
+//kullanıcıdan veri almak
+//prompt ==> string olarak geliyor.
+var userData=prompt("lütfen bir kelime giriniz")
+document.write(userData);
+
+
 
 
