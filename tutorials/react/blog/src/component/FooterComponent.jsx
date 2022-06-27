e377ed25c1ab85cf34809619aec36a1f22612e98
hamitmizrak <hamitmizrak@gmail.com>
2022-06-27 20:53:03 +0300

    Footer

--- tutorials/react/blog/src/component/FooterComponent.jsx
+++ tutorials/react/blog/src/component/FooterComponent.jsx
@@ -3,59 +3,57 @@ import React from 'react'
 export default function FooterComponent() {
   return (
     <>
-    <footer class="bg-light text-center text-white">
-  <div class="container p-4 pb-0">
-    <section class="mb-4">
+    <footer className="bg-light text-center text-white">
+  <div className="container p-4 pb-0">
+    <section className="mb-4">
       <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #3b5998;"
+        className="btn btn-primary btn-floating m-1"
+        style= {{backgroundColor:"#3b5998"}}
         href="#!"
         role="button"
-        ><i class="fab fa-facebook-f"></i
+        ><i className="fab fa-facebook-f"></i
       ></a>
       <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #55acee;"
+        className="btn btn-primary btn-floating m-1"
+        style=  {{backgroundColor:"#55acee"}}
         href="#!"
         role="button"
-        ><i class="fab fa-twitter"></i
+        ><i className="fab fa-twitter"></i
       ></a>
       <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #dd4b39;"
+        className="btn btn-primary btn-floating m-1"
+        style={{backgroundColor:"#dd4b39"}}
         href="#!"
         role="button"
-        ><i class="fab fa-google"></i
+        ><i className="fab fa-google"></i
       ></a>
       <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #ac2bac;"
+        className="btn btn-primary btn-floating m-1"
+        style={{backgroundColor:"#ac2bac"}}
         href="#!"
         role="button"
-        ><i class="fab fa-instagram"></i
+        ><i className="fab fa-instagram"></i
       ></a>
       <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #0082ca;"
+        className="btn btn-primary btn-floating m-1"
+        style={{backgroundColor:"#0082ca"}}
         href="#!"
         role="button"
-        ><i class="fab fa-linkedin-in"></i
+        ><i className="fab fa-linkedin-in"></i
       ></a>
       <a
-        class="btn btn-primary btn-floating m-1"
-        style="background-color: #333333;"
+        className="btn btn-primary btn-floating m-1"
+        style={{backgroundColor:"#333333"}}
         href="#!"
         role="button"
-        ><i class="fab fa-github"></i
+        ><i className="fab fa-github"></i
       ></a>
     </section>
   </div>
-
-
-
-  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
-    © 2020 Copyright:
-    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+ 
+  <div className="text-center p-3"  style={{backgroundColor:"rgba(0, 0, 0)"}}>
+    &copy; 2022 Copyright:
+    <a className="text-white" href="https://">TeachCareer- Tubisad</a>
   </div>
 </footer>
     </>
