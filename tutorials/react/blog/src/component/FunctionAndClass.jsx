01906c970265f526cb28f272f2f7fad17e762672
hamitmizrak <hamitmizrak@gmail.com>
2022-06-27 20:17:30 +0300

    react

---
+++ tutorials/react/blog/src/component/FunctionAndClass.jsx
@@ -0,0 +1,26 @@
+// function(sabit veriler için kullan)
+import React from 'react'
+
+export default function FunctionAndClass() {
+  return (
+    <>
+    FunctionAndClass
+    </>
+  )
+}
+
+
+// dinamik eriler için kullan
+import React, { Component } from 'react'
+
+export default class FunctionAndClass extends Component {
+  render() {
+    return (
+      <div>FunctionAndClass</div>
+    )
+  }
+}
+
+
+
+
