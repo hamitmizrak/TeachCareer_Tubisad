01dee3d6cd5b9a533aca991113e9b36a77b0b13f
hamitmizrak <hamitmizrak@gmail.com>
2022-06-27 20:48:02 +0300

    Footer MDB

--- tutorials/react/blog/src/App.js
+++ tutorials/react/blog/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import HeaderComponent from './component/HeaderComponent';
+import FooterComponent from './component/FooterComponent';
 
 function App() {
   return (
     <>
      <HeaderComponent/>
+     <FooterComponent/>
     </>
   );
 }
---
+++ tutorials/react/blog/src/component/FooterComponent.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+export default function FooterComponent() {
+  return (
+    <>
+    <footer class="bg-light text-center text-white">
+  <div class="container p-4 pb-0">
+    <section class="mb-4">
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #3b5998;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-facebook-f"></i
+      ></a>
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #55acee;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-twitter"></i
+      ></a>
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #dd4b39;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-google"></i
+      ></a>
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #ac2bac;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-instagram"></i
+      ></a>
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #0082ca;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-linkedin-in"></i
+      ></a>
+      <a
+        class="btn btn-primary btn-floating m-1"
+        style="background-color: #333333;"
+        href="#!"
+        role="button"
+        ><i class="fab fa-github"></i
+      ></a>
+    </section>
+  </div>
+
+
+
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    © 2020 Copyright:
+    <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+</footer>
+    </>
+  )
+}
