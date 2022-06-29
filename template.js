0b0d738a6b0734a315c6578a843e9bf2a9552b52
hamitmizrak <hamitmizrak@gmail.com>
2022-06-29 18:25:40 +0300

    wow animate

--- template.js
+++ template.js
@@ -1,3 +1,36 @@
+
+    $( function() {
+      var availableTags = [
+        "ActionScript",
+        "AppleScript",
+        "Asp",
+        "BASIC",
+        "C",
+        "C++",
+        "Clojure",
+        "COBOL",
+        "ColdFusion",
+        "Erlang",
+        "Fortran",
+        "Groovy",
+        "Haskell",
+        "Java",
+        "JavaScript",
+        "Lisp",
+        "Perl",
+        "PHP",
+        "Python",
+        "Ruby",
+        "Scala",
+        "Scheme"
+      ];
+      $("#tags").autocomplete({
+        source: availableTags
+      });
+    } );
+
+
+
 let date = () => {
   //alert("deneme")
 
